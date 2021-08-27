c7e44d709e646c46b4bfde2523370787f1ac97fb
Ian Salazar <iisalazar@up.edu.ph>
2021-08-27 23:26:40 +0800

    task: created READ, UPDATE, DELETE, and LIST endpoint for Institution Entity
    - LIST endpoint has an optional flag using query paramters to get nearby institutions
    - UPDATE endpoints has a TODO: add input sanitation / validation

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { SampleEndpoint } from "@endpoints";
+import { SampleEndpoint, InstitutionEndpoint } from "@endpoints";
 export const router = Router();
 
 // place your base endpoints here
 router.use("/sample", SampleEndpoint);
+router.use("/institution", InstitutionEndpoint);
---
+++ src/utils/location.ts
@@ -0,0 +1,6 @@
+/**
+ *
+ * @param d in km
+ * @returns in degree
+ */
+export const distanceToDegConverter = (d: number): number => (d * 0.1) / 11.11;
