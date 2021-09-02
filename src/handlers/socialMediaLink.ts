b9323e9fcdb78ac909d8c2dae5cc311474fe2c1f
rladomingo <radomingo4@up.edu.ph>
2021-09-02 23:05:41 +0800

    Edit handlers for social media links
    - Added type string to id parameter
    
    - Adjust indentation spaces to match

--- src/handlers/socialMediaLink.ts
+++ src/handlers/socialMediaLink.ts
@@ -2,40 +2,40 @@ import { Institution, SocialMediaLink } from "@models";
 import { errors } from "@constants";
 
 interface LinkBody {
-  institutionId?: number;
-  institution?: Institution;
-  url?: string;
-  type?: string;
+    institutionId?: number;
+    institution?: Institution;
+    url?: string;
+    type?: string;
 }
 
 export class SocialMediaLinkHandler {
-  async getLinks(institutionId: number): Promise<SocialMediaLink[]> {
-    const link = await SocialMediaLink.find({ institutionId });
-    return link;
-  }
+    async getLinks(institutionId: number): Promise<SocialMediaLink[]> {
+        const link = await SocialMediaLink.find({ institutionId });
+        return link;
+    }
 
-  async create(institutionId: number, options: LinkBody): Promise<SocialMediaLink> {
-    const link = new SocialMediaLink();
-    Object.assign(link, {
-      institutionId,
-      ...options,
-    });
-    await link.save();
-    return link;
-  }
+    async create(institutionId: number, options: LinkBody): Promise<SocialMediaLink> {
+        const link = new SocialMediaLink();
+        Object.assign(link, {
+            institutionId,
+            ...options,
+        });
+        await link.save();
+        return link;
+    }
 
-  async update(id: number, options: LinkBody): Promise<SocialMediaLink> {
-    const link = await SocialMediaLink.findOne(id);
-    if (!link) throw new Error(errors.NOT_FOUND);
-    Object.assign(link, { ...options });
-    await link.save();
-    return link;
-  }
+    async update(id: number | string, options: LinkBody): Promise<SocialMediaLink> {
+        const link = await SocialMediaLink.findOne(id);
+        if (!link) throw new Error(errors.NOT_FOUND);
+        Object.assign(link, { ...options });
+        await link.save();
+        return link;
+    }
 
-  async delete(id: number): Promise<boolean> {
-    const link = await SocialMediaLink.findOne(id);
-    if (!link) throw new Error(errors.NOT_FOUND);
-    await link.softRemove();
-    return true;
-  }
+    async delete(id: number | string): Promise<boolean> {
+        const link = await SocialMediaLink.findOne(id);
+        if (!link) throw new Error(errors.NOT_FOUND);
+        await link.softRemove();
+        return true;
+    }
 }
