993d769754cac7ea9821b6dcbc93774a0be925b4
mtnakashima <mtnakashima@up.edu.ph>
2021-09-02 15:59:14 +0800

    task: add CRUDL Handlers for Tag Entity

---
+++ src/handlers/tag.ts
@@ -0,0 +1,51 @@
+import { Story, Tag } from '@models';
+import { errors } from '@constants';
+
+interface TagBody {
+    storyId?: number;
+    story?: Story;
+    text?: string;
+}
+
+export class TagHandler {
+    async getTagsbyStoryId(storyId: number): Promise<Tag[]> {
+        const tag = await Tag.find({ storyId });
+        return tag;
+    }
+
+    async getTag(id: number | string): Promise<Tag> {
+        const tag = await Tag.findOne(id); 
+        if (!tag) throw new Error (errors.NOT_FOUND); 
+        return tag;
+    }
+
+    async createTag(storyId: number, options: TagBody): Promise<Tag> {
+        const tag = new Tag(); 
+        Object.assign(tag, {
+            storyId,
+            ...options,
+        });
+        await tag.save(); 
+
+        return tag;
+    }
+
+    async updateTag(id: number | string, options: TagBody): Promise<Tag> {
+        const tag = await Tag.findOne(id); 
+        if (!tag) throw new Error(errors.NOT_FOUND);
+
+        Object.assign(tag, {...options});
+        await tag.save();
+
+        return tag;
+    }
+
+    async deleteTag(id: number | string): Promise<boolean> {
+        const tag = await Tag.findOne(id); 
+        if (!tag) throw new Error(errors.NOT_FOUND); 
+
+        await tag.softRemove();
+
+        return true;
+    }
+}
