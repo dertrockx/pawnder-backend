9565d4995de522eb06ac974877856dfb1f229f93
Lea Marie Somoson <lbsomoson1@up.edu.ph>
2021-09-05 09:14:03 -0700

    resolve merge conflict issue

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,8 +1,5 @@
 export * from "./sample";
 export * from "./institution";
 export * from "./story";
-<<<<<<< HEAD
-export * from "./user";
-=======
 export * from "./socialMediaLink";
->>>>>>> 31349c8c5ced37066d077e5beef7e589a6181ffe
+export * from "./user";
