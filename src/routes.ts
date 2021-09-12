b02fee24f03e02ccc034d2dfe901e0210711a8da
Ian Salazar <iisalazar@up.edu.ph>
2021-09-12 10:45:55 +0800

    task: created CRUDL endpoint for pet and photo
    - basic CRUDL operations for pets
    - CRUD endpoint for photo
    - I created two separate endpoints for each entity because when a client wants to update or upload a new photo, they don't have to change the pet's information

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { SampleEndpoint, InstitutionEndpoint, StoryEndpoint } from "@endpoints";
+import {
+	SampleEndpoint,
+	InstitutionEndpoint,
+	StoryEndpoint,
+	PetEndpoint,
+	PhotoEndpoint,
+} from "@endpoints";
 export const router = Router();
 
 // place your base endpoints here
 router.use("/sample", SampleEndpoint);
 router.use("/institution", InstitutionEndpoint);
 router.use("/story", StoryEndpoint);
+router.use("/pet", PetEndpoint);
+router.use("/photo", PhotoEndpoint);
