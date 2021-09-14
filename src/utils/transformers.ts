10e94fe7b93bf6781458e3e98283ef033461fc0f
Ian Salazar <iisalazar@up.edu.ph>
2021-09-14 19:25:45 +0800

    seed: updated seed script to include password field

--- src/utils/transformers.ts
+++ src/utils/transformers.ts
@@ -1,6 +1,9 @@
 import { ValueTransformer } from "typeorm";
+import { hashSync } from "bcrypt";
+
+export const SALT_ROUNDS = 10;
 
 export const transformHashedValue: ValueTransformer = {
-	to: (password: string): string => password,
+	to: (password: string): string => hashSync(password, SALT_ROUNDS),
 	from: (hash) => hash,
 };
