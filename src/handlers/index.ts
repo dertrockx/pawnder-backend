07805a5ee8026a3a08991a25b43eab756e0690b9
rladomingo <radomingo4@up.edu.ph>
2021-09-20 15:40:28 +0800

    Resolve merge conflicts

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -3,8 +3,6 @@ export * from "./institution";
 export * from "./story";
 export * from "./tag";
 export * from "./pet";
-export * from "./photo";
-export * from "./application";
 export * from "./auth";
 export * from "./ignored";
 export * from "./socialMediaLink";
