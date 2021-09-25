b1eed4f06e00d26d1f1fc764ea04a86b8317b996
Lea Marie Somoson <lbsomoson1@up.edu.ph>
2021-09-25 23:18:45 +0800

    allow CORS

--- src/app.ts
+++ src/app.ts
@@ -20,6 +20,13 @@ export class App {
 		this.app.use(express.json());
 		this.app.use(express.urlencoded({ extended: true }));
 		this.app.use(morgan("dev"));
+		this.app.use((req, res, next) => {
+			res.setHeader("Access-Control-Allow-Origin", 'http://localhost:3000');
+			res.setHeader("Access-Control-Allow-Methods", 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+			res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+			res.setHeader("Access-Control-Allow-Credentials", 'true');
+			next();
+		})
 	};
 
 	private configureRoutes = (): void => {
