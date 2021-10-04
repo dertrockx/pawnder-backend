e4177e45fb5631992503c95adad1c05adf543ad2
Ian Salazar <iisalazar@up.edu.ph>
2021-10-04 15:06:43 +0800

    updated config to handle production configuration

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,21 +1,25 @@
 import { createConnection } from "typeorm";
+import { ConnectionOptions } from "typeorm";
 import * as Entities from "@models";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
-export const connection = () =>
-	createConnection({
-		type: "mysql",
-		host: "localhost",
-		port: 3306,
-		synchronize: true,
-		logging: false,
-		dropSchema: false,
+let config: ConnectionOptions = {
+	type: "mysql",
+	host: process.env.MYSQL_HOST || "localhost",
+	port: parseInt(process.env.MYSQL_PORT) || 3306,
+	synchronize: true,
+	logging: false,
+	dropSchema: false,
 
-		username: process.env.MYSQL_USERNAME || "pawnder",
-		password: process.env.MYSQL_PASSWORD || "pawnder",
-		database: process.env.MYSQL_DATABASE || "pawnder",
+	username: process.env.MYSQL_USERNAME || "pawnder",
+	password: process.env.MYSQL_PASSWORD || "pawnder",
+	database: process.env.MYSQL_DATABASE || "pawnder",
+	entities: [...Object.values(Entities)],
+};
 
-		entities: [...Object.values(Entities)],
-	});
+const env = process.env.NODE_ENV || "development";
+if (env === "production")
+	Object.assign(config, { type: "mysql", url: process.env.MYSQL_CONNECT_URL });
+export const connection = () => createConnection(config);
