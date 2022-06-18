1dcea1f5553243edf60e91daff58744f4fa1c20a
Ian Salazar <iisalazar@up.edu.ph>
2022-06-18 16:55:42 +0800

    feat: added basic documentation w/ swagger

--- src/app.ts
+++ src/app.ts
@@ -3,53 +3,61 @@ import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import { router } from "./routes";
 import { allowedMethods } from "@middlewares";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
 export class App {
-  public app: express.Express;
-  public port: number;
+	public app: express.Express;
+	public port: number;
 
-  constructor() {
-    this.app = express();
-    this.port = this.getPort();
+	constructor() {
+		this.app = express();
+		this.port = this.getPort();
 
-    this.configureServer();
-    this.configureRoutes();
-  }
+		this.configureServer();
+		this.configureRoutes();
+	}
 
-  private getPort = (): number => +process.env.PORT || 8081;
+	private getPort = (): number => +process.env.PORT || 8081;
 
-  private configureServer = (): void => {
-    this.app.use(express.json());
-    this.app.use(express.urlencoded({ extended: true }));
-    this.app.use(morgan("dev"));
-    this.app.use((req, res, next) => {
-      res.setHeader(
-        "Access-Control-Allow-Origin",
-        process.env.FRONTEND_URL || "http://localhost:3000"
-      );
-      res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-      );
-      res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
-      );
-      res.setHeader("Access-Control-Allow-Credentials", "true");
-      next();
-    });
-    this.app.use(cookieParser());
-  };
+	private configureServer = (): void => {
+		this.app.use(express.json());
+		this.app.use(express.urlencoded({ extended: true }));
+		this.app.use(morgan("dev"));
+		this.app.use((req, res, next) => {
+			res.setHeader(
+				"Access-Control-Allow-Origin",
+				process.env.FRONTEND_URL || "http://localhost:3000"
+			);
+			res.setHeader(
+				"Access-Control-Allow-Methods",
+				"GET,HEAD,OPTIONS,POST,PUT,DELETE"
+			);
+			res.setHeader(
+				"Access-Control-Allow-Headers",
+				"Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+			);
+			res.setHeader("Access-Control-Allow-Credentials", "true");
+			next();
+		});
+		this.app.use(cookieParser());
 
-  private configureRoutes = (): void => {
-    this.app.use("/api/0.1", router);
-  };
+		this.app.use(
+			"/api-docs",
+			swaggerUi.serve,
+			swaggerUi.setup(swaggerDocument)
+		);
+	};
 
-  public start = (): void => {
-    console.log("^ Starting application");
-    this.app
-      .listen(this.port, (): void => {
-        console.log(`> Listening on port ${this.port}`);
-      })
-      .on("error", (err) => console.log(err));
-  };
+	private configureRoutes = (): void => {
+		this.app.use("/api/0.1", router);
+	};
+
+	public start = (): void => {
+		console.log("^ Starting application");
+		this.app
+			.listen(this.port, (): void => {
+				console.log(`> Listening on port ${this.port}`);
+			})
+			.on("error", (err) => console.log(err));
+	};
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -5,64 +5,64 @@ import { UserIgnoredPet } from "./UserIgnoredPet";
 import { transformHashedValue } from "@utils";
 @Entity()
 export class User extends DefaultEntity {
-	@Column()
-	email: string;
+  @Column()
+  email: string;
 
-	@Column({ select: false, transformer: transformHashedValue })
-	password: string;
+  @Column({ select: false, transformer: transformHashedValue })
+  password: string;
 
-	@Column({ nullable: true })
-	firstName: string;
+  @Column({ nullable: true })
+  firstName: string;
 
-	@Column({ nullable: true })
-	middleName: string;
+  @Column({ nullable: true })
+  middleName: string;
 
-	@Column({ nullable: true })
-	lastName: string;
+  @Column({ nullable: true })
+  lastName: string;
 
-	@Column({ type: "timestamp", nullable: true })
-	birthDate: Date;
+  @Column({ type: "timestamp", nullable: true })
+  birthDate: Date;
 
-	@Column({
-		type: "enum",
-		enum: SexEnum,
-		default: SexEnum.Male,
-		nullable: true,
-	})
-	sex: SexEnum;
+  @Column({
+    type: "enum",
+    enum: SexEnum,
+    default: SexEnum.Male,
+    nullable: true,
+  })
+  sex: SexEnum;
 
-	@Column({ nullable: true })
-	photoUrl: string;
+  @Column({ nullable: true })
+  photoUrl: string;
 
-	@Column({ nullable: true })
-	contactNumber: string;
+  @Column({ nullable: true })
+  contactNumber: string;
 
-	@Column({ type: "float", nullable: true })
-	locationLat: number;
+  @Column({ type: "float", nullable: true })
+  locationLat: number;
 
-	@Column({ type: "float", nullable: true })
-	locationLong: number;
+  @Column({ type: "float", nullable: true })
+  locationLong: number;
 
-	// User preferences
-	@Column({
-		type: "enum",
-		enum: AnimalTypeEnum,
-		default: AnimalTypeEnum.Dogs,
-		nullable: true,
-	})
-	preferredAnimal: AnimalTypeEnum;
+  // User preferences
+  @Column({
+    type: "enum",
+    enum: AnimalTypeEnum,
+    default: AnimalTypeEnum.Dogs,
+    nullable: true,
+  })
+  preferredAnimal: AnimalTypeEnum;
 
-	@Column({
-		type: "enum",
-		enum: ActionEnum,
-		default: ActionEnum.Adopt,
-		nullable: true,
-	})
-	action: ActionEnum;
+  @Column({
+    type: "enum",
+    enum: ActionEnum,
+    default: ActionEnum.Adopt,
+    nullable: true,
+  })
+  action: ActionEnum;
 
-	@Column({ type: "float", nullable: true })
-	preferredDistance: number;
+  @Column({ type: "float", nullable: true })
+  preferredDistance: number;
 
-	@OneToMany(() => UserIgnoredPet, (ignoredPet) => ignoredPet.user)
-	ignoredPets: UserIgnoredPet[];
+  @OneToMany(() => UserIgnoredPet, (ignoredPet) => ignoredPet.user)
+  ignoredPets: UserIgnoredPet[];
 }
